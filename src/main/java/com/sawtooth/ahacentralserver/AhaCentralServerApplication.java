c0846fa8941f685d174bca5ebadd4810061d9211
Sawtooth04 <andrey.y96@mail.ru>
2023-12-26 10:54:37 +0300

    Initial commit.

---
+++ src/main/java/com/sawtooth/ahacentralserver/AhaCentralServerApplication.java
@@ -0,0 +1,11 @@
+package com.sawtooth.ahacentralserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhaCentralServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AhaCentralServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sawtooth/ahacentralserver/AhaCentralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawtooth.ahacentralserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhaCentralServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
